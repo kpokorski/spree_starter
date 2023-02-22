682fe1ec4b00c57508d85e30cf1c4b8e1dee01b4
kpokorski <kfpokorski@gmail.com>
2023-02-22 11:18:21 +0100

    user_decorator and user view displaying image

---
+++ app/controllers/my_store/spree/users_controller_decorator.rb
@@ -0,0 +1,19 @@
+module MyStore
+  module Spree
+    module UsersControllerDecorator
+      #user.profile_picture.attach(params[:profile_picture]) if params[:profile_picture]
+
+      def user_params
+        @user.profile_picture.attach(params[:profile_picture])
+        permitted_user_attributes.append(:profile_picture)
+        params.require(:user).permit(permitted_user_attributes |
+                                       [:use_billing,
+                                        spree_role_ids: [],
+                                        ship_address_attributes: permitted_address_attributes,
+                                        bill_address_attributes: permitted_address_attributes])
+      end
+    end
+  end
+end
+
+::Spree::Admin::UsersController.prepend MyStore::Spree::UsersControllerDecorator if ::Spree::Admin::UsersController.included_modules.exclude?(MyStore::Spree::UsersControllerDecorator)
--- db/migrate/20230220114051_change_string_to_be_attachment_in_user_profile_picture.rb
+++
@@ -1,5 +0,0 @@
-class ChangeStringToBeAttachmentInUserProfilePicture < ActiveRecord::Migration[7.0]
-  def change
-    change_column :spree_users, :profile_picture, :attachment
-  end
-end
